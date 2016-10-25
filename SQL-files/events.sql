33c16c27d76ded1f260bc73503eb275c010a0366
nartherious <frostwolfe@gmail.com>
2016-10-25 19:18:35 -0400

    Update events.sql

--- SQL-files/events.sql
+++ SQL-files/events.sql
@@ -43,6 +43,29 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
+INSERT INTO 'events' VALUES 
+  (1,'Park Cleanup Westville NJ','Open','Looking for volunteers to help clean up XYZ park',
+    'Westville, NJ 08093','07/13/2017','7AM-4PM','312','John Smith','smith@email.com'),
+  (2,'Test Event 2','Closed','This is a description for Test Event 2',
+    'Glassboro, NJ 08028','02/01/2017','10AM-7PM','47','John Wayne','wayne@email.com'),
+  (3,'Test Event 3','Open','This is a description for Test Event 3',
+    'Glassboro, NJ 08028','03/23/2017','10AM-7PM','47','John Stamos','stamos@email.com'),
+  (4,'Test Event 4','Open','This is a description for Test Event 4',
+    'Glassboro, NJ 08028','04/14/2017','10AM-7PM','47','John Hancock','hancock@email.com'),
+  (5,'Test Event 5','Closed','This is a description for Test Event 5',
+    'Glassboro, NJ 08028','05/15/2017','10AM-7PM','47','John F Kennedy','kennedy@email.com'),
+  (6,'Test Event 6','Open','This is a description for Test Event 6',
+    'Glassboro, NJ 08028','06/16/2017','10AM-7PM','47','John D Rockefeller','rockefeller@email.com'),
+  (7,'Test Event 7','Open','This is a description for Test Event 7',
+    'Glassboro, NJ 08028','07/17/2017','10AM-7PM','47','John Mellencamp','mellencamp@email.com'),
+  (8,'Test Event 8','Closed','This is a description for Test Event 8',
+    'Glassboro, NJ 08028','08/18/2017','10AM-7PM','47','John Calvin','calvin@email.com'),
+  (9,'Test Event 9','Closed','This is a description for Test Event 9',
+    'Glassboro, NJ 08028','09/19/2017','10AM-7PM','47','John J. Pershing','pershing@email.com'),
+  (10,'Test Event 10','Open','This is a description for Test Event 10',
+    'Glassboro, NJ 08028','10/10/2017','10AM-7PM','47','John Cabot','cabot@email.com'),
+  (11,'Test Event 11','Open','This is a description for Test Event 11',
+    'Glassboro, NJ 08028','11/11/2017','10AM-7PM','47','John Cena','ucantcme@email.com');
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
